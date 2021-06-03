458283b5b02b9beba6b152d4ad8d722cd9fed6ec
ferasdiab <dfiras96@gmail.com>
2021-06-03 11:21:56 +0300

    create products router

---
+++ backend/routers/controllers/product.js
@@ -0,0 +1,80 @@
+const productModel = require("../../db/models/products");
+
+const getAllProducts = (req,res)=>{
+  productModel.find({}).populate("comment").exec().then((result)=>{
+    res.status(200).json(result);
+  }).catch((err) => {
+    res.send(err)
+  });
+}
+///////////////////////////
+const createProduct = (req, res) => {
+  const { type, name, price, img, description, quantity } = req.body;
+  const newProduct = new productModel({ type, name, price, img, description, quantity })
+  newProduct.save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+///////////////
+const getProductById = (req,res)=>{
+ 
+  const _id = req.params.id
+  productModel.findOne({_id}).then((result)=>{
+   
+    res.status(200).json(result);
+  }).catch((err) => {
+    res.status(404).json("not found");
+  });
+
+};
+////////////
+const getProductByType = (req,res)=>{
+ 
+  const type = req.params.type
+  productModel.find({type}).then((result)=>{
+    if (result.length === 0 ){
+      res.status(404).json("not found");
+      return
+    }
+    res.status(200).json(result);
+  }).catch((err) => {
+    res.status(404).json("not found");
+  });
+
+};
+
+
+///////////////
+const updateProductById = (req,res)=>{
+ 
+  const _id = req.params.id
+ // const { type, name, price, img, description, quantity } = req.body;
+  productModel.findByIdAndUpdate(_id,req.body,{new:true}).then((result)=>{
+    res.status(200).json(result);
+  }).catch((err) => {
+    res.status(404).json("not found");
+  });
+
+};
+
+const deleteProductById  = (req,res)=>{
+  const _id = req.params.id
+  productModel.findOneAndDelete({_id}).then((result)=>{
+    if (!result ){
+      res.status(404).json("not found");
+      return
+    }
+    res.status(200).json(result);
+  }).catch((err) => {
+    res.status(404).json("not found");
+  });
+}
+
+
+module.exports = {
+  getAllProducts,createProduct,getProductById,getProductByType,updateProductById,deleteProductById
+};
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const roleRouter = require('./routers/routes/role')
 const createUserRouter = require("../backend/routers/routes/createUser")
 const loginRouter = require("./routers/routes/login")
-const createProductRouter = require("./routers/routes/createProduct")
+const productsRouter = require("./routers/routes/product")
 const  createNewCommentRouter = require("./routers/routes/createComment")
 
 //built-in middlewares
@@ -23,8 +23,9 @@ app.use(cors());
 app.use(roleRouter);
 app.use(createUserRouter);
 app.use(loginRouter);
-app.use(createProductRouter);
+app.use('/products', productsRouter);
 app.use(createNewCommentRouter);
+
 
 /////////////////////
 const PORT = process.env.PORT || 5000;
