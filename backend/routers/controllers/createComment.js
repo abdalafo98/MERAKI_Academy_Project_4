4e7047ab86467e5493689835dbd63a5826715b0f
ferasdiab <dfiras96@gmail.com>
2021-06-04 18:59:44 +0300

    ambrove added to cart and fav to be for login user

--- backend/routers/controllers/createComment.js
+++ backend/routers/controllers/createComment.js
@@ -1,24 +1,26 @@
 const commentModel = require("../../db/models/comments");
-const productModel = require("../../db/models/products")
-
+const productModel = require("../../db/models/products");
 
 const createComment = (req, res) => {
-  const {comment} = req.body
-  const id = req.params.id
-  const commenter = req.token.userId
-  const newComment = new commentModel({comment,commenter})
-  newComment.save().then(async (result)=>{
-    await productModel.updateOne(
-        { _id: id }, 
+  const { comment } = req.body;
+  const id = req.params.id;
+  const commenter = req.token.userId;
+  const newComment = new commentModel({ comment, commenter });
+  newComment
+    .save()
+    .then(async (result) => {
+      await productModel.updateOne(
+        { _id: id },
         { $push: { comment: result._id } }
-    );
+      );
 
-    res.status(201)
-    res.json(result)
-  }).catch((err) => {
+      res.status(201);
+      res.json(result);
+    })
+    .catch((err) => {
       res.send(err);
     });
 };
 module.exports = {
-    createComment,
+  createComment,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./db/db');
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/db");
 
 const app = express();
 
 //routers
 
-const roleRouter = require('./routers/routes/role')
-const userRouter = require("../backend/routers/routes/createUser")
-const loginRouter = require("./routers/routes/login")
-const productsRouter = require("./routers/routes/product")
-const  createNewCommentRouter = require("./routers/routes/createComment")
-const  createRatingRouter = require("./routers/routes/rating")
-const favoritesRouter = require("./routers/routes/favoritesRouter")
-const cartRouter = require("./routers/routes/cartRouter")
+const roleRouter = require("./routers/routes/role");
+const userRouter = require("./routers/routes/userRouter");
+const loginRouter = require("./routers/routes/login");
+const productsRouter = require("./routers/routes/product");
+const createNewCommentRouter = require("./routers/routes/createComment");
+const createRatingRouter = require("./routers/routes/rating");
+const favoritesRouter = require("./routers/routes/favoritesRouter");
+const cartRouter = require("./routers/routes/cartRouter");
 
 //built-in middlewares
 app.use(express.json());
@@ -24,19 +24,16 @@ app.use(cors());
 //app routers
 
 app.use(roleRouter);
-app.use('/user',userRouter);
+app.use("/user", userRouter);
 app.use(loginRouter);
-app.use('/products', productsRouter);
+app.use("/products", productsRouter);
 app.use(createNewCommentRouter);
 app.use(createRatingRouter);
-app.use("/favorites",favoritesRouter)
-app.use("/cart",cartRouter)
-
-
-
+app.use("/favorites", favoritesRouter);
+app.use("/cart", cartRouter);
 
 /////////////////////
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server On ${PORT}`);
+  console.log(`Server On ${PORT}`);
 });
