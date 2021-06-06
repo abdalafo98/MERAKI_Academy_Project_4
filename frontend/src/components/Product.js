f024be0e75295f922217f6df8ac1e174b87e6913
ferasdiab <dfiras96@gmail.com>
2021-06-06 19:34:22 +0300

    fix some pug in comment

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,22 +1,36 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory, useParams } from "react-router-dom";
 
 export default function Product() {
   const { id } = useParams();
-  const [result, setResult] = useState("");
-  axios
-    .get(`http://localhost:5000/products/id/${id}`)
-    .then((result) => {
-      console.log(result.data);
-      setResult(result.data);
-    })
-    .catch((err) => {
-      console.log(err);
+  const [result, setResult] = useState([]);
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/products/id/${id}`)
+      .then((response) => {
+        setResult(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  let arr = "";
+  if (result.comment) {
+    arr = result.comment.map((element, i) => {
+      return (
+        <div className="comment">
+          <p>{element.commenter.firstName}</p>
+          <p>{element.comment}</p>
+        </div>
+      );
     });
+  }
+
   return (
     <>
-      <div className="">
+      <div className="containerDiv">
         <div className="productHolder">
           <div className="img">
             <img src={result.img} style={{ width: "400px" }} />
@@ -34,7 +48,7 @@ export default function Product() {
           </div>
         </div>
         <div>
-          <section> rating and comments</section>
+          <section>{arr}</section>
         </div>
       </div>
     </>
