990623b2cdccafa15bec0f8d58c3742cdae40584
ferasdiab <dfiras96@gmail.com>
2021-06-06 10:23:06 +0300

    start work day 1

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import { Link, Route } from "react-router-dom";
 
-export default function Navigation({ token , setToken }) {
-    const signOut = ()=>{
-        setToken("")
-        localStorage.clear()
-    };
+export default function Navigation({ token, setToken }) {
+  const signOut = () => {
+    setToken("");
+    localStorage.clear();
+  };
   return (
     <div className="Navigation">
       <Link to="/">Home</Link>
-      {!token ? <Link to="/register">Register</Link> : ""}
       {!token ? <Link to="/login">Login</Link> : ""}
-      {token ? <button onClick={signOut} >Sign Out</button> : ""}
+      {!token ? <Link to="/register">Register</Link> : ""}
+      {token ? <button onClick={signOut}>Sign Out</button> : ""}
     </div>
   );
 }
