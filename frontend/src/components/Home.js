8b0e79c77f6a2f0d67ce2eaf147e0a8cdf4a911a
ferasdiab <dfiras96@gmail.com>
2021-06-06 13:50:47 +0300

    fix some pug in home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -9,18 +9,18 @@ export default function Home() {
     const moveToMen =()=>{
         history.push("/men");
     }
+    const moveToKids =()=>{
+        history.push("/kid");
+    }
     const moveToWomen =()=>{
         history.push("/women");
-    }
-    const moveToKids =()=>{
-        history.push("/kids");
     }
     return (
         <div className="containerDiv">
             <div className="homeHeader">
                 <div className="sectionNav"onClick={moveToMen}>Men</div>
-                <div className="sectionNav" onClick={moveToWomen}>Kids</div>
-                <div className="sectionNav" onClick={moveToKids}>Womens</div>
+                <div className="sectionNav" onClick={moveToKids}>Kids</div>
+                <div className="sectionNav" onClick={moveToWomen}>Women</div>
             </div>
             <div className="slideshowContainer">
 
