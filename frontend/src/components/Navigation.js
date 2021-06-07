c2555c245fdc4df7bda4312d6f8ef03361f1083e
user name <abdalafo.abubaker.com>
2021-06-07 09:29:37 +0300

    add styling

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
-import { Link, Route } from "react-router-dom";
-
+import { Link, Route, useHistory } from "react-router-dom";
+import Cart from "./../../src/cart.png";
 export default function Navigation({ token, setToken }) {
+  const history = useHistory();
   const signOut = () => {
     setToken("");
     localStorage.clear();
   };
 
-  const ssds = () => {
-    return;
-  };
   return (
     <div className="Navigation">
-      <ul>
-        <li>
-          {" "}
-          <Link to="/">Home</Link>
-        </li>
-        <li> {!token ? <Link to="/login">Login</Link> : ""}</li>
-        <li> {token ? <a onClick={signOut}>Sign Out</a> : ""}</li>
-      </ul>
+      <header>
+        {" "}
+        <h1
+          class="logo"
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          THE <span> MOUNTAIN</span>
+        </h1>
+        <input type="text" placeholder="Search.."></input>
+        <nav>
+          <ul className="nav">
+            <li>
+              {" "}
+              <Link to="/">Home</Link>
+            </li>
+            <li> {token ? <Link to="/cart">cart</Link> : ""}</li>
+            <li> {token ? <Link to="/favorites">favorites</Link> : ""}</li>
+            <li> {token ? <a onClick={signOut}>Sign Out</a> : ""}</li>{" "}
+            <li> {!token ? <Link to="/login">Login</Link> : ""}</li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
