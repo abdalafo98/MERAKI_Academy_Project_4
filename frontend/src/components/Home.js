5db49248faf0b6b4246c80ec0f05734ab58f2827
ferasdiab <dfiras96@gmail.com>
2021-06-09 18:55:07 +0300

    restoreOurWork

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,10 +2,13 @@ import { React, useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { useHistory, Route, Redirect } from "react-router-dom";
 import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
+import 'react-slideshow-image/dist/styles.css';
+
 
 export default function Home() {
+ 
   const [slideImages, setslideImages] = useState([]);
+ 
 
   const history = useHistory();
   const moveToMen = () => {
@@ -18,9 +21,7 @@ export default function Home() {
     history.push("/women");
   };
 
-  const terms = () => {
-    history.push("/women");
-  };
+  
 
   useEffect(() => {
     axios
@@ -31,30 +32,10 @@ export default function Home() {
       .catch((err) => {
         console.log(err);
       });
+
+  
   }, []);
   // end of slider show
-
-  //Fotter section//
- const moveToAboutUs  =()=>{
-
-  history.push("/aboutUs")
- }
-  const moveToReturnP  =()=>{
-
-    history.push("/returnPolicy")
-  }
-  const moveToOurStaff  =()=>{
-
-    history.push("/ourStaff")
-  }
-  const moveToPrivacy     =()=>{
-
-    history.push("/privacyandPolicy")
-  }
-  const moveToContactUs   =()=>{
-
-    history.push("/contactUs")
-  }
 
   return (
     <div>
@@ -73,67 +54,48 @@ export default function Home() {
       <div className="slide-container">
         <Slide>
           <div className="each-slide">
-            <div
-              className="imageSlider"
-              style={{ backgroundImage: `url(${slideImages[0]})` }}
-            ></div>
+            <div className="imageSlider" style={{ 'backgroundImage': `url(${slideImages[0]})`  }}>
+
+            </div>
           </div>
           <div className="each-slide">
-            <div
-              className="imageSlider"
-              style={{ backgroundImage: `url(${slideImages[1]})` }}
-            ></div>
+            <div className="imageSlider" style={{ 'backgroundImage': `url(${slideImages[1]})` }}>
+
+            </div>
           </div>
           <div className="each-slide">
-            <div
-              className="imageSlider"
-              style={{ backgroundImage: `url(${slideImages[2]})` }}
-            ></div>
+            <div className="imageSlider" style={{ 'backgroundImage': `url(${slideImages[2]})` }}>
+
+            </div>
           </div>
           <div className="each-slide">
-            <div
-              className="imageSlider"
-              style={{ backgroundImage: `url(${slideImages[3]})` }}
-            ></div>
+            <div className="imageSlider" style={{ 'backgroundImage': `url(${slideImages[3]})` }}>
+
+            </div>
           </div>
           <div className="each-slide">
-            <div
-              className="imageSlider"
-              style={{ backgroundImage: `url(${slideImages[4]})` }}
-            ></div>
+            <div className="imageSlider" style={{ 'backgroundImage': `url(${slideImages[4]})` }}>
+            </div>
           </div>
         </Slide>
       </div>
       <div className="homebody">
-        <div
-          className="sectionNavBody"
-          onClick={moveToMen}
-          style={{
-            backgroundImage: `url(https://jade-media.jadeblue.com/media/wysiwyg/jadeblue/jadeblue-new.jpg)`,
-          }}
-        ></div>
-        <div
-          className="sectionNavBody"
-          onClick={moveToKids}
-          style={{
-            backgroundImage: `url(https://i.pinimg.com/originals/61/47/12/614712f5f6f0ea8e80c77e9ed9bc2359.jpg)`,
-          }}
-        ></div>
-        <div
-          className="sectionNavBody"
-          onClick={moveToWomen}
-          style={{
-            backgroundImage: `url(https://cdn.shopify.com/s/files/1/0020/9544/8117/files/VOF_Veil_of_Faith_Banners_6002dce5-18be-40d8-811d-94bc50beda18_x800.png?v=1622703826)`,
-          }}
-        ></div>
+      <div className="sectionNavBody" onClick={moveToMen} style={{'backgroundImage': `url(https://jade-media.jadeblue.com/media/wysiwyg/jadeblue/jadeblue-new.jpg)`}}>
+       
+        </div>
+        <div className="sectionNavBody" onClick={moveToKids} style={{'backgroundImage': `url(https://i.pinimg.com/originals/61/47/12/614712f5f6f0ea8e80c77e9ed9bc2359.jpg)`}}>
+         
+        </div>
+        <div className="sectionNavBody" onClick={moveToWomen} style={{'backgroundImage': `url(https://cdn.shopify.com/s/files/1/0020/9544/8117/files/VOF_Veil_of_Faith_Banners_6002dce5-18be-40d8-811d-94bc50beda18_x800.png?v=1622703826)`}}>
+         
+        </div>
       </div>
-      <div className="homefooter">
-        <div style={{ cursor: "pointer" }}onClick={moveToAboutUs}>About Us</div>
-        <div style={{ cursor: "pointer" }}onClick={moveToReturnP}>Return Policy</div>
-        <div style={{ cursor: "pointer" }}onClick={moveToOurStaff}>Our Staff</div>
-        <div style={{ cursor: "pointer" }}onClick={moveToPrivacy}>Privacy & Policys</div>
-        <div style={{ cursor: "pointer" }}onClick={moveToContactUs}>Contact Us</div>
+      <div className="homefotter">
+      <h3>  <span>about us</span></h3>
+       <h3> <span>contact us</span></h3>
       </div>
+
+     
     </div>
   );
 }
