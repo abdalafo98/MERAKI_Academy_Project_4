e06c0da3d1c6d338ed680aacf0c3f6e58098a6ff
user name <abdalafo.abubaker.com>
2021-06-10 14:20:36 +0300

    modify profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -10,7 +10,11 @@ const Profile = () => {
   let thisToken = localStorage.getItem("token");
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/user/id/${id}`)
+      .get(`http://localhost:5000/user/id/`, {
+        headers: {
+          authorization: "Bearer " + thisToken,
+        },
+      })
       .then((result) => {
         setResult(result.data);
         console.log(result);
@@ -23,7 +27,48 @@ const Profile = () => {
 
   return (
     <div>
-      <h1>Full Name:{result.firstName}</h1>
+      <div className="user-info">
+        <div>
+          <img
+            src={
+              result.gender === "male"
+                ? "https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png"
+                : "https://image.freepik.com/free-vector/people-profile-icon_24877-40756.jpg"
+            }
+            height={200}
+            width={200}
+          />
+        </div>
+        <div>
+          <p>
+            <span> Full Name: </span>
+            {result.firstName} {result.lastName}
+          </p>
+          <p>
+            {" "}
+            <span>age: </span>
+            {result.age}
+          </p>
+          <p>
+            {" "}
+            <span>Phone Number: </span>
+            {result.phoneNumber}
+          </p>
+          <p>
+            {" "}
+            <span>Email: </span>
+            {result.email}
+          </p>
+          <p>
+            {" "}
+            <span>Gender:</span> {result.gender}
+          </p>
+          <p>
+            {" "}
+            <span>country:</span> {result.country}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
