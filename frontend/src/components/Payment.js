9fe990a613b097a0656f55633b238005aa8c5023
ferasdiab <dfiras96@gmail.com>
2021-06-11 14:52:35 +0300

    addStyleForPayment

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -1,67 +1,119 @@
-import React,{ useState,  } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Cards from 'react-credit-cards';
-import 'react-credit-cards/es/styles-compiled.css';
-
-
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import "./Payment.css";
 
 export default function Payment() {
   let thisToken = localStorage.getItem("token");
-const  newArr  = JSON.parse(localStorage.getItem("newArr"))
-const totalPrice =parseInt( localStorage.getItem("totalPrice"))
-const [number, setNumber] = useState("");
-const [name, setName] = useState("");
-const [expiry, setExpiry] = useState("");
-const [cvc, setCvc] = useState("");
-const [focused, setFocused] = useState("");
+  const newArr = JSON.parse(localStorage.getItem("newArr"));
+  const totalPrice = parseInt(localStorage.getItem("totalPrice"));
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focused, setFocused] = useState("");
 
-console.log("newArr",newArr);
-console.log(typeof(totalPrice));
-const payNow = ()=>{
-    const date = Date().split(" GMT+0300 (Eastern European Summer Time)")[0].replaceAll(" ","-")
-    axios.post("http://localhost:5000/order",{
-      date , products:newArr , totalPrice : totalPrice 
-    },{
-       headers: {
-      authorization: "Bearer " + thisToken,
-     },
-     }).then((result)=>{
-       console.log(result);
-     }).catch((err)=>{
-       console.log(err);
-     })
-}
-    /*
+  console.log("newArr", newArr);
+  console.log(typeof totalPrice);
+  const payNow = () => {
+    const date = Date()
+      .split(" GMT+0300 (Eastern European Summer Time)")[0]
+      .replaceAll(" ", "-");
+    axios
+      .post(
+        "http://localhost:5000/order",
+        {
+          date,
+          products: newArr,
+          totalPrice: totalPrice,
+        },
+        {
+          headers: {
+            authorization: "Bearer " + thisToken,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  /*
     
     */
-    return (
-        <>
-        <div>
-        <div>
-           <input placeholder="security Code"  />
-           <h4>inter your card details to continue with your payment</h4>
-           
-         
-           <input type="checkbox"PayPal />
+  return (
+    <>
+      <div>
+        <div className="h4">
+          <h4>inter your credit Card details to continue with your payment</h4>
         </div>
-        <div>
-            <input name="number" placeholder ='number' onChange={(e)=>{setNumber(e.target.value) }} onFocus={(e)=>{setFocused(e.target.name) }} />
-            <input name="name" placeholder="Name" onChange={(e)=>{setName(e.target.value)} }  onFocus={(e)=>{setFocused(e.target.name) }}    />
-            <input name ="expiry" placeholder="MM/YY expiry"  onChange={(e)=>{setExpiry(e.target.value)} } onFocus={(e)=>{setFocused(e.target.name) }} />
-            <input name="cvc" placeholder="Cvc" onChange={(e)=>{setCvc(e.target.value)} }  onFocus={(e)=>{setFocused(e.target.name) }}  />
+        <div className="payment-input">
+          <div>
+            <input
+              name="number"
+              placeholder="number"
+              onChange={(e) => {
+                setNumber(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              name="name"
+              placeholder="Name"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              name="expiry"
+              placeholder="MM/YY expiry"
+              onChange={(e) => {
+                setExpiry(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              name="cvc"
+              placeholder="Cvc"
+              onChange={(e) => {
+                setCvc(e.target.value);
+              }}
+              onFocus={(e) => {
+                setFocused(e.target.name);
+              }}
+            />
+          </div>
         </div>
+        <div></div>
         <div>
-            <button onClick = {payNow}>Confirm Payment</button>
+          <Cards
+            number={number}
+            name={name}
+            expiry={expiry}
+            cvc={cvc}
+            focused={focused}
+          />
         </div>
-        <div>
-        <Cards
-      number={number}
-      name={name}
-      expiry={expiry}
-      cvc={cvc}
-      focused={focused}/>
+        <div className = "button">
+          <button onClick={payNow}>Confirm Payment</button>
+        </div>
       </div>
-        </div>
-        </>
-    )
+    </>
+  );
 }
